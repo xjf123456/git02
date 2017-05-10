bca23960138dbbada2d72004c15a379aa031e4d6
xjf123456 <1254089513@qq.com>
2017-05-10 17:13:32 +0800

    飞机

--- git/git/Test.java
+++ git/git/Test.java
@@ -4,6 +4,6 @@ public class Test {
 //sad1f564asd5f4asd4f456sad4f564szd654f561SD
 //4fasz65d4f8zsd65f456as156f56sdz1f561zs56f15s
 	public static void main(String[] args) {
-		System.out.println("sb");
+		System.out.println("mdzz");
 	}
 }
